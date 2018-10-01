9747770b4e1715b9641d8795bf303dc3b9624965
Christian <chriamue@gmail.com>
2018-10-01 10:34:11 +0200

    updated backend

--- pytorch_unet_backend.py
+++ pytorch_unet_backend.py
@@ -2,125 +2,168 @@ from __future__ import absolute_import
 import os
 import numpy as np
 import cv2
+from tqdm import tqdm
 
 import torch
-from torch import optim, nn
+import torch.nn as nn
 import torch.nn.functional as F
-from .unet import UNet
-from .utils import batch, hwc_to_chw
+from torch.utils import data
 
 from protoseg.backends import AbstractBackend
 from protoseg.trainer import Trainer
 
 from tensorboardX import SummaryWriter
 
+from .unet import UNet
+
 
 class pytorch_unet_backend(AbstractBackend):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    dummy_input = None  # used for onnx export
+    graph_exported = False
 
     def __init__(self):
-        AbstractBackend.__init__(self)
+        super(pytorch_unet_backend, self).__init__()
 
     def load_model(self, config, modelfile):
-        model = UNet(n_channels=3, n_classes=1)
-        if config['gpu']:
-            model.cuda()
+        model = UNet(n_channels=3, n_classes=config['classes']).to(self.device)
         if os.path.isfile(modelfile):
             print('loaded model from:', modelfile)
-            model.load_state_dict(torch.load(modelfile))
+            model = torch.load(modelfile)
+        model = torch.nn.DataParallel(
+            model, device_ids=range(torch.cuda.device_count()))
+        self.dummy_input = None
+        self.graph_exported = False
         return model
 
     def save_model(self, model):
-        torch.save(model.model.state_dict(),
+        torch.save(model.model,
                    model.modelfile)
         print('saved model to:', model.modelfile)
 
+    def get_optimizer(self, name, parameters, config):
+        optimizer = None
+        if name == 'sgd':
+            optimizer = torch.optim.SGD(parameters,
+                                        lr=config['learn_rate'])
+        elif name == 'adadelta':
+            optimizer = torch.optim.Adadelta(parameters,
+                                             lr=config['learn_rate'])
+        elif name == 'adagrad':
+            optimizer = torch.optim.Adagrad(
+                parameters, lr=config['learn_rate'])
+        elif name == 'adam':
+            optimizer = torch.optim.Adam(parameters, lr=config['learn_rate'])
+        elif name == 'rmsprop':
+            optimizer = torch.optim.RMSprop(
+                parameters, lr=config['learn_rate'])
+        else:
+            optimizer = torch.optim.SGD(parameters,
+                                        lr=config['learn_rate'])
+        return optimizer
+
+
     def init_trainer(self, trainer):
-        trainer.loss_function = nn.BCELoss()
-        trainer.optimizer = optim.SGD(trainer.model.model.parameters(),
-                          lr=trainer.config['learn_rate'],
-                          momentum=0.9,
-                          weight_decay=0.0005)
+        trainer.loss_function = nn.CrossEntropyLoss().to(self.device)
+        trainer.optimizer = torch.optim.SGD(trainer.model.model.parameters(), lr=trainer.config['learn_rate'],
+                            momentum=0.9,
+                            weight_decay=5e-4)
+        trainer.model.model.train()
 
     def dataloader_format(self, img, mask=None):
         if img.ndim == 2:
             img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-        img = hwc_to_chw(img)
+        img = np.transpose(img, axes=[2, 0, 1])
         if mask is None:
             return img.astype(np.float32)
-
         if mask.ndim == 3:
             mask = cv2.cvtColor(mask, cv2.COLOR_RGB2GRAY)
-        return img.astype(np.float32), mask.astype(np.float32)
+
+        mask[mask > 0] = 1  # binary mask
+        img = img.astype(np.float32)
+        mask = mask.astype(np.int64)
+        return torch.from_numpy(img), torch.from_numpy(mask)
 
     def train_epoch(self, trainer):
-        print('train on pytorch_unet_backend')
         batch_size = trainer.config['batch_size']
         summarysteps = trainer.config['summarysteps']
-
         epoch_loss = 0
 
-        for i, b in enumerate(batch(trainer.dataloader, batch_size)):
+        dataloader = data.DataLoader(
+            trainer.dataloader, batch_size=batch_size, num_workers=min(batch_size, 8), shuffle=True
+        )
+
+        for (images, labels) in tqdm(dataloader):
             trainer.global_step += 1
-            imgs = np.array([i[0] for i in b])
-            true_masks = np.array([i[1] for i in b])
-
-            imgs = torch.from_numpy(imgs)
-            true_masks = torch.from_numpy(true_masks)
-
-            if trainer.config['gpu']:
-                imgs = imgs.cuda()
-                true_masks = true_masks.cuda()
-
-            masks_pred = trainer.model.model(imgs)
-            masks_probs = torch.sigmoid(masks_pred)
-            masks_probs_flat = masks_probs.view(-1)
-
-            true_masks_flat = true_masks.view(-1)
-
-            loss = trainer.loss_function(masks_probs_flat, true_masks_flat)
-            epoch_loss += loss.item()
-
-            if i % summarysteps == 0:
-                print('{0:.4f} --- loss: {1:.6f}'.format(i *
-                                                         batch_size / len(trainer.dataloader), loss.item()))
-                trainer.summarywriter.add_scalar('loss', loss.item(), global_step=trainer.global_step)
-                trainer.summarywriter.add_image('image', imgs[0], global_step=trainer.global_step)
-                trainer.summarywriter.add_image('mask', true_masks[0], global_step=trainer.global_step)
-                trainer.summarywriter.add_image('predicted', masks_probs[0], global_step=trainer.global_step)
+            trainer.model.model.train()
+            if self.dummy_input is None:
+                self.dummy_input = images.to(torch.device('cpu'))
+            images = images.to(self.device)
+            labels = labels.to(self.device)
 
             trainer.optimizer.zero_grad()
+            outputs = trainer.model.model(images)
+
+            loss = trainer.loss_function(input=outputs, target=labels)
+
             loss.backward()
             trainer.optimizer.step()
+            trainer.loss += loss.item()
 
+            if trainer.global_step % summarysteps == 0:
+                print('{0:.4f} --- loss: {1:.6f}'.format(trainer.global_step *
+                                                         batch_size / len(trainer.dataloader), loss.item()))
+                if trainer.summarywriter:
+                    trainer.summarywriter.add_scalar(
+                        trainer.name+'loss', loss.item(), global_step=trainer.global_step)
+                    trainer.summarywriter.add_image(
+                        trainer.name+'image', images[0], global_step=trainer.global_step)
+                    trainer.summarywriter.add_image(
+                        trainer.name+'mask', labels[0], global_step=trainer.global_step)
+                    pred = outputs.data.max(1)[1].cpu().numpy()
+                    trainer.summarywriter.add_image(
+                        trainer.name+'predicted', pred[0], global_step=trainer.global_step)
+                    if not self.graph_exported:
+                        try:
+                            trainer.summarywriter.add_graph(
+                                trainer.model.model, images)
+                            self.graph_exported = True
+                        except Exception as e:
+                            print(e)
 
     def validate_epoch(self, trainer):
         batch_size = trainer.config['batch_size']
-        dataloader = trainer.valdataloader
-        for i, (X_batch, y_batch) in enumerate(dataloader):
-            #prediction = self.batch_predict(trainer, X_batch)
-            trainer.summarywriter.add_image(
-                "val_image", (X_batch[0]/255.0), global_step=trainer.epoch)
-            trainer.summarywriter.add_image(
-                "val_mask", (y_batch[0]/255.0), global_step=trainer.epoch)
-            #trainer.summarywriter.add_image(
-            #    "val_predicted", (prediction), global_step=trainer.epoch)
+        dataloader = data.DataLoader(
+            trainer.valdataloader, batch_size=batch_size, num_workers=min(batch_size, 8), shuffle=True
+        )
+        for i, (X_batch, y_batch) in tqdm(enumerate(dataloader)):
+            prediction = self.batch_predict(trainer, X_batch)
+            trainer.metric(
+                prediction[0], y_batch[0].numpy(), prefix=trainer.name)
+            if trainer.summarywriter:
+                trainer.summarywriter.add_image(
+                    trainer.name+"val_image", (X_batch[0]/255.0), global_step=trainer.epoch)
+                trainer.summarywriter.add_image(
+                    trainer.name+"val_mask", (y_batch[0]), global_step=trainer.epoch)
+                trainer.summarywriter.add_image(
+                    trainer.name+"val_predicted", (prediction[0]), global_step=trainer.epoch)
 
     def get_summary_writer(self, logdir='results/'):
         return SummaryWriter(log_dir=logdir)
 
     def predict(self, predictor, img):
-        img_batch = batchify.Stack()([img])
+        img_batch =torch.from_numpy(np.array([img]))
         return self.batch_predict(predictor, img_batch)
 
     def batch_predict(self, predictor, img_batch):
         model = predictor.model.model
+
         try:
             model = model.module
         except Exception:
             pass
-        with autograd.predict_mode():
-            outputs = model(img_batch.as_in_context(self.ctx))
-            output, _ = outputs
-        predict = mxnet.nd.squeeze(
-            mxnet.nd.argmax(output, 1)).asnumpy().clip(0, 1)
-        return predict.astype(np.float32)
+        model.eval()
+        images = img_batch.to(self.device)
+        outputs = model(images)
+        pred = outputs.data.max(1)[1].cpu().numpy()
+        return pred
